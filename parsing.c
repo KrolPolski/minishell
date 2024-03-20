1e11ae32cf9a45cd3b6c8fb6a119c9825bef3abe
Ryan Boudwin <rboudwin@student.hive.fi>
2024-03-20 13:53:20 +0200

    >> now appends as intended

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 14:36:57 by akovalev          #+#    #+#             */
-/*   Updated: 2024/03/20 12:36:58 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/03/20 13:51:06 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -366,7 +366,7 @@ t_cmd*	parseredirs(t_cmd *cmd, char **ps, char *es)
 		else if (tok == '>')
 			cmd = redircmd(cmd, q, eq, O_WRONLY | O_CREAT | O_TRUNC, 1);
 		else if (tok == '+')
-			cmd = redircmd(cmd, q, eq, O_WRONLY | O_CREAT, 1);
+			cmd = redircmd(cmd, q, eq, O_WRONLY | O_CREAT | O_APPEND, 1);
 	}
 	return (cmd);
 }

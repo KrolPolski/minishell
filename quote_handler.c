1382423c0152d4dfd0bc8fbcc5bacb4253778dea
Andrey Kovalevskiy <akovalev@student.hive.fi>
2024-03-20 17:46:19 +0200

    quote handler appears to handle everything correctly including env expansion, though maybe more edge cases need to be considered

--- quote_handler.c
+++ quote_handler.c
@@ -6,7 +6,7 @@
 /*   By: akovalev <akovalev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 16:49:02 by akovalev          #+#    #+#             */
-/*   Updated: 2024/03/19 18:55:22 by akovalev         ###   ########.fr       */
+/*   Updated: 2024/03/20 17:41:02 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@ char	*fetch_env_var(char *var, char **env)
 	char	*eq_ptr;
 
 	i = 0;
-
 	while (env[i])
 	{
 		eq_ptr = ft_strchr(env[i], '=');
 		if (ft_strlen(var) - 1 == eq_ptr - env[i])
-			if (ft_strnstr(env[i], var + 1, ft_strlen(var) - 1)) //offsets are to account for $
-				return (env[i] + ft_strlen(var));//to get past VAR=
+			if (ft_strnstr(env[i], var + 1, ft_strlen(var) - 1))
+				return (env[i] + ft_strlen(var));
 		i++;
 	}
 	return (NULL);
@@ -50,19 +49,20 @@ char	*replace_name(char *ptr, char *var, char *exp_var, char *beg_str, char **st
 	memcpy(upd_str + (ptr - beg_str) + new_vlen, ptr + old_vlen, \
 		old_strlen - (ptr - beg_str) - old_vlen + 1);
 	*str = upd_str + (ptr - beg_str) + new_vlen - 1;
-	//printf("str is now at: %s\n", *str);
 	free(beg_str);
 	return (upd_str);
 }
 
-char	*expand_env(char **beg_str, char *str, char **env)
+char	*expand_env(char **beg_str, char *str, char **env, char **begq, char **endq)
 {
 	char	*end_str;
 	char	*ptr;
 	char	*var;
 	char	*exp_var;
 	char	whitespace[] = " \t\r\n\v\'\"";
+	int		diff;
 
+	diff = *begq - *beg_str;
 	end_str = *beg_str + ft_strlen(*beg_str);
 	ptr = str;
 	while (str < end_str && !ft_strchr(whitespace, *str))
@@ -71,54 +71,81 @@ char	*expand_env(char **beg_str, char *str, char **env)
 	ft_strlcpy(var, ptr, str - ptr + 1);
 	exp_var = fetch_env_var(var, env);
 	if (exp_var != NULL)
+	{
 		*beg_str = replace_name(ptr, var, exp_var, *beg_str, &str);
+		if (*begq && *endq)
+		{
+			*begq = *beg_str + diff;
+			//printf("begq is moved to %s\n", *begq);
+			*endq = ft_strchr(*begq + 1, '\"');
+			// *begq = ft_strchr(*beg_str, '\"');
+			// *endq = ft_strchr(*begq + 1, '\"');
+		}
+	}
 	free(var);
 	return (str);
+}
+
+void	remove_quotes(char *begq, char *endq)
+{
+	ft_memmove(begq, begq + 1, ft_strlen(begq));
+	ft_memmove(endq - 1, endq, ft_strlen(endq - 1));
 }
 
 char	*quote_handler(char *str, char **env)
 {
 	bool	sfl;
 	bool	dfl;
-	char	*endq;
+	char	*endsq;
+	char	*enddq;
+	char	*begsq;
+	char	*begdq;
 	char	*beg_str;
 
 	beg_str = str;
-	sfl = 0;
-	dfl = 0;
-
 	while (*str)
 	{
 		if (*str == '\'' && dfl != 1)
 		{
-			if (sfl == 0 && ft_strchr(str + 1, '\''))
+			if (sfl == 0)
 			{
-				sfl = 1;
-				//printf("Opening single\n");
-			}
-			else if (sfl == 1)
-			{
-				sfl = 0;
-				//printf("Closing single\n");
+				endsq = ft_strchr(str + 1, '\'');
+				if (endsq)
+				{
+					begsq = str;
+					sfl = 1;
+				}
 			}
 		}
 		else if (*str == '\"' && sfl != 1)
 		{
-			endq = ft_strchr(str + 1, '\"');
-			if (dfl == 0 && endq)
+			if (dfl == 0)
 			{
-				dfl = 1;
-				//printf("Opening double\n");
-			}
-			else if (dfl == 1)
-			{
-				dfl = 0;
-				//printf("Closing double\n");
+				enddq = ft_strchr(str + 1, '\"');
+				if (enddq)
+				{
+					begdq = str;
+					dfl = 1;
+				}
 			}
 		}
 		else if (*str == '$' && sfl != 1)
+			str = expand_env(&beg_str, str, env, &begdq, &enddq);
+		if (endsq != NULL && (str == endsq))
 		{
-			str = expand_env(&beg_str, str, env);
+			remove_quotes(begsq, endsq);
+			sfl = 0;
+			endsq = NULL;
+			str--;
+			str--;
+		}
+		if (enddq != NULL && (str == enddq))
+		{
+			remove_quotes(begdq, enddq);
+			dfl = 0;
+			enddq = NULL;
+			str--;
+			str--;
 		}
 		str++;
 	}

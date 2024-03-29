25971abf8dbf16051beb14f7f2532b4b639c17a5
Ryan Boudwin <rboudwin@student.hive.fi>
2024-03-29 15:23:08 +0200

    unset now handles multiple valid arguments

--- built_in2.c
+++ built_in2.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:30:34 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/03/29 12:59:39 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/03/29 15:22:24 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,37 +142,81 @@ void	ft_exit(t_execcmd *ecmd, t_info *info)
 	exit(exit_code);
 }
 
+char	*search_matrix(char *arg, char **matrix, int *i, int curr_len)
+{
+	char *arg_plus;
+	*i = 0;
+
+	
+	arg_plus = ft_strjoin(arg, "=");
+	ft_printf("about to search for '%s' inside '%s'\n", arg_plus, matrix[*i]);
+	while (*i < curr_len)
+	{
+		ft_printf("about to search for '%s' inside '%s'\n", arg_plus, matrix[*i]);
+		if (!matrix[*i])
+			(*i)++;
+		else if (!ft_strncmp(matrix[*i], arg_plus, ft_strlen(arg_plus)))
+			{
+				ft_printf("we found it baby\n");
+				free(arg_plus);
+				return (matrix[*i]);
+			}
+		else
+			(*i)++;
+	}
+	free(arg_plus);
+	ft_printf("about to return NULL");
+	return (NULL);
+}
 void	ft_unset(t_execcmd *ecmd, t_info *info)
 {
-	char	**new_env;
-	int		curr_len;
-	int		i;
 	int		k;
+	char	*str;
+	int		i;
+	char	*targets;
+	char	curr_len;
+	char	**new_env;
+	int		a;
+	int		b;
 
+	
+	k = 1;
 	curr_len = ft_matrix_len(info->curr_env);
-	ft_printf("current length of curr_env is %d\n", curr_len);
-	new_env = malloc(sizeof(char *) * (curr_len));
-	i = 0;
-	while (info->curr_env[i] && ft_strncmp(info->curr_env[i], ecmd->argv[1], ft_strlen(ecmd->argv[1])))
+	while (ecmd->argv[k])
 	{
-		new_env[i] = info->curr_env[i];
-		i++;
+		ft_printf("about to search the matrix for an unset\n");
+		str = search_matrix(ecmd->argv[k], info->curr_env, &i, curr_len);
+		ft_printf("we exited search_matrix\n");
+		// if we find a bad one we just wanna move on
+		if (!str)
+		{
+			ft_printf("we concluded that %s isn't actually in the matrix so moving on\n", ecmd->argv[k]);
+			k++;
+		}
+		else
+		{
+			ft_printf("we found %s in the matrix so we are freeing it\n", ecmd->argv[k]);
+			free(info->curr_env[i]);
+			info->curr_env[i] = NULL;
+			i = 0;
+			k++;
+		}
 	}
-	if (info->curr_env[i])
+	new_env = malloc(sizeof(char *) * curr_len + 1);
+	if (!new_env)
+		exit(1);
+	a = 0;
+	b = 0;
+	while (b < curr_len)
 	{
-		free(info->curr_env[i]); //why are we getting free errors here? it should be leaking without it
-		info->curr_env[i] = NULL;
-		k = i;
-		i++;
+		if (info->curr_env[b])
+		{
+			new_env[a] = info->curr_env[b];
+			a++;
+		}		
+		b++;	
 	}
-	while (info->curr_env[i])
-	{
-		new_env[k] = info->curr_env[i];
-		k++;
-		i++;
-	}
-	new_env[i] = NULL;
+	new_env[++a] = NULL;
 	free(info->curr_env);
 	info->curr_env = new_env;
-	//need to consider the case where unset is run against something that doesn't actually exist.
 }

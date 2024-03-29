6217ef7df1b345ba1ecce5967224f11ebc240b72
Ryan Boudwin <rboudwin@student.hive.fi>
2024-03-29 10:50:38 +0200

    export now supports declaring more than one variable at a time

--- built_in2.c
+++ built_in2.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 11:30:34 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/03/26 16:21:14 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/03/29 10:49:37 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,29 @@ void	ft_export(t_execcmd *ecmd, t_info *info)
 	char	**new_env;
 	int		curr_len;
 	int		i;
+	int		k;
 
+	k = 1;
 	curr_len = ft_matrix_len(info->curr_env);
 	ft_printf("current length of curr_env is %d\n", curr_len);
-	new_env = malloc(sizeof(char *) * (curr_len + 2)); // adding one for NULL and one for the new value
+	new_env = malloc(sizeof(char *) * (curr_len + 1 + ft_matrix_len(&ecmd->argv[k]))); // adding one for NULL and one for the new value
 	i = 0;
 	while (info->curr_env[i])
+		{
+			new_env[i] = info->curr_env[i];
+			i++;
+		}
+	while (ecmd->argv[k])
 	{
-		new_env[i] = info->curr_env[i];
+		
+		new_env[i] = ft_strdup(ecmd->argv[k]);
+		ft_printf("new_env[i] should now have a new value: %s\n", new_env[i]);
 		i++;
+		k++;
 	}
-	new_env[i] = ft_strdup(ecmd->argv[1]);
-	ft_printf("new_env[i] should now have a new value: %s\n", new_env[i]);
-	i++;
-	new_env[i] = NULL;
-	free(info->curr_env);
-	info->curr_env = new_env;
+		new_env[++i] = NULL;
+		free(info->curr_env);
+		info->curr_env = new_env;
 	//ft_printf("ecmd->argv[0] is %s and ecmd->argv[1] is %s\n", ecmd->argv[0], ecmd->argv[1]);
 }
 /* if exit code is provided as an argument, exits the shell with it.

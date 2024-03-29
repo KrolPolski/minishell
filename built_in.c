02386940f8c9df81bcea967ff1992a14a6a671c0
Ryan Boudwin <rboudwin@student.hive.fi>
2024-03-29 13:07:22 +0200

    export with no args no longer runs in parent process

--- built_in.c
+++ built_in.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 10:09:19 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/03/29 12:37:55 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/03/29 13:04:41 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,8 +98,8 @@ void	handle_builtins(t_execcmd *ecmd, char **env,
 		ft_echo(ecmd, info);
 	if (!ft_strncmp(builtin_command, "env", ft_strlen(builtin_command)))
 		ft_env(ecmd, info);
-	//if (!ft_strncmp(builtin_command, "export", ft_strlen(builtin_command)))
-	//	ft_export(ecmd, info); we may not want this running in the child processes
+	if (!ft_strncmp(builtin_command, "export", ft_strlen(builtin_command)))
+		ft_export(ecmd, info); //we may not want this running in the child processes
 	if (!ft_strncmp(builtin_command, "exit", ft_strlen(builtin_command)))
 		ft_exit(ecmd, info);
 	if (!ft_strncmp(builtin_command, "unset", ft_strlen(builtin_command)))
